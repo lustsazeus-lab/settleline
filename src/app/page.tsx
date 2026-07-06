0757bff82be0d475584137e473400670d8f3f165
Luna Ops <lustsazeus@gmail.com>
2026-07-07 00:18:13 +0800

    chore: scaffold SettleLine MVP

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <main>
+      <h1>SettleLine</h1>
+      <p>Verifiable World Cup settlement demo powered by TxLINE-shaped data.</p>
+    </main>
+  );
+}
